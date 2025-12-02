a671fab807bb6f0317362b66d0eb5794c5d089de
Adam Maus <adam.maus@wisc.edu>
2025-12-02 15:08:17 -0600

    Removed old line-endings.

--- app/models/user.rb
+++
@@ -1,6 +0,0 @@
-class User < ApplicationRecord
-  has_secure_password
-
-  validates :username, presence: true
-  belongs_to :role
-end
--- script/db-container-startup.sql
+++
@@ -1,2 +0,0 @@
--- Needed to properly install database
-SET GLOBAL FOREIGN_KEY_CHECKS=0;
